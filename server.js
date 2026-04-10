472f6394282de50b1cfa2215ca5a013626170223
TaifoorAnsari <mohammadtaifoor9175@gmail.com>
2026-04-10 19:27:12 +0530

    corrected cors

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.static(__dirname));
-app.use(cors());
+app.use(cors({
+  origin: "https://ngo-connect-ten.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
